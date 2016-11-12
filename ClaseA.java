0e658e76c0068e97a9e620f9a9148c02c15ef969
pocasangre77 <pocasangre52@hotmail.com>
2016-11-12 17:13:59 -0600

    Modificacion 2
    Se creo un metodo

--- ClaseA.java
+++ ClaseA.java
@@ -1,10 +1,13 @@
 public claseA{
-private int x;
+private String x;
  private int y;
  
 
 public ClaseA(){
-x = 0;
+x = "";
  y = 0;
   }
+ public void mensaje(){
+  System.out.println("Hola");
+}
 }
